eab4c036e0f35f572bc31756c56dac88c7c4a5f2
zhfeg <847738436@qq.com>
2018-04-01 08:42:12 +0000

    Initial commit

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('static_pages.default')
+
+@section('content')
+<h1>关于页</h1>
+@stop
+@section('title','关于')
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('static_pages.default')
+
+@section('content')
+<h1>帮助页</h1>
+@stop
+@section('title','帮助')
