344a00331ed233e73ce9debaa57a9af58542bfc9
zhfeg <847738436@qq.com>
2018-04-11 11:37:15 +0000

    Updated database configuration

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+     public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
--- resources/views/static_pages/default.blade.php
+++ resources/views/static_pages/default.blade.php
@@ -6,10 +6,13 @@
 </head>
 <body>
   @include('layouts._header')
-
+ 
     <div class="container">
       <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+
         @yield('content')
+
         @include('layouts._footer')
       </div>
     </div>
